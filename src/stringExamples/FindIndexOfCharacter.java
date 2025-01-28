aeb83ebfa79bfa339e77d36c564ff382024a7a71
Mohammadabdulrab <mohammadabdulrab78760@gmail.com>
2025-01-28 14:44:45 +0530

    Add index of the character

---
+++ src/stringExamples/FindIndexOfCharacter.java
@@ -0,0 +1,21 @@
+package stringExamples;
+import java.util.Scanner;
+public class FindIndexOfCharacter {
+
+    public static int returnIndex(String str){
+        int index=-1;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='r'){
+                index=i;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(returnIndex(str));
+
+
+    }
+}
